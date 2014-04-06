f607b163029b4957c98278b7d13785be8da51c24
Biplob Chandra Sarkar <biplob.weblancer@gmail.com>
2014-04-06 12:02:16 +0600

    pn...

--- js/push.js
+++ js/push.js
@@ -105,13 +105,14 @@
 						//$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
 						//$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
 						
-						if(e.payload.message == 'Reschedule') {
-							
-							//alert(e.payload.message);
+						if(e.payload.message == 'Job Accepted') {
+							//alert(e.payload.message+"Zahid");
 							
 							$('#popupDialog').popup('close');
-							urlString = "scheduleinfo.html";
+							urlString = "rate-taxi.html";
 							window.open(urlString);
+						}else if( e.payload.message == 'Taxi Arrived'){
+							alert('Taxi Arrived');
 						}
 						
                     break;
